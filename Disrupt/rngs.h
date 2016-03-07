414bd9ff45162f82a962068528c54fe8cc939216
Connor Hallett <connor.hallett@gmail.com>
2016-03-07 12:48:57 -0500

    Added missing header file for random number generator

---
+++ Disrupt/rngs.h
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------
+ * Name            : rngs.h  (header file for the library file rngs.c)
+ * Author          : Steve Park & Dave Geyer
+ * Language        : ANSI C
+ * Latest Revision : 09-22-98
+ * -----------------------------------------------------------------------
+ */
+
+#if !defined( _RNGS_ )
+#define _RNGS_
+
+#pragma acc routine seq
+int Random(int max, long *seeds);
+#endif
